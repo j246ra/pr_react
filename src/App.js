fc939462439c425a865c2b475ccb487684dd1a5b
j.nishimura <junya.nishimura.0915@gmail.com>
2023-04-12 14:00:19 +0900

    Test API Request.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { execTest } from "./lib/api/test";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const handleExecTest = async () => {
+    const res = await execTest()
+
+    if (res.status === 200) {
+      setMessage(res.data.message)
+    }
+  }
+
+  useEffect(() => {
+    handleExecTest()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{message}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
