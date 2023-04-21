a63d78eb1557f6e251a770f9457a03ecb1d640be
j.nishimura <junya.nishimura.0915@gmail.com>
2023-04-21 13:16:07 +0900

    Add Sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import './App.css';
+import './App.scss';
 import Login from "./Login";
 import SignUp from "./SignUp";
 import Hello from "./Hello";
