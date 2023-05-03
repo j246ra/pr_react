d7868437f08d52fa5131bfa9cb5343bb062858bd
j.nishimura <junya.nishimura.0915@gmail.com>
2023-05-03 17:34:38 +0900

    BrowserRouter を createBrowserRouter に変更

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
-import { Routes, Route } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import './App.scss';
 import "./session.scss";
-import LifelogApiProvider from "./providers/LifelogApiProvider";
-import Login from "./Login";
-import SignUp from "./SignUp";
-import Hello from "./Hello";
-import AccountUpdate from "./AccountUpdate";
 import Header from "./Header";
-import PasswordForget from "./PasswordForget";
-import PasswordEdit from "./PasswordEdit";
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
-      <LifelogApiProvider>
-        <Toaster />
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={<Hello />}
-          />
-          <Route
-            path="/login"
-            element={<Login />}
-          />
-          <Route
-            path="/sign_up"
-            element={<SignUp />}
-          />
-          <Route
-            path="/update_account"
-            element={<AccountUpdate />}
-          />
-          <Route
-            path="/password_forget"
-            element={<PasswordForget />}
-          />
-          <Route
-            path="/password_edit"
-            element={<PasswordEdit />}
-          />
-          <Route
-            path="/hello"
-            element={<Hello />}
-          />
-        </Routes>
-      </LifelogApiProvider>
+      <Toaster />
+      <Header />
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router} from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import './index.css';
 import UserProvider from "./providers/UserProvider";
 import AuthApiProvider from "./providers/AuthApiProvider";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {CookiesProvider} from "react-cookie";
+import LifelogApiProvider from "./providers/LifelogApiProvider";
+import Hello from "./Hello";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import AccountUpdate from "./AccountUpdate";
+import PasswordForget from "./PasswordForget";
+import PasswordEdit from "./PasswordEdit";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Hello />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/sign_up" element={<SignUp />}/>
+      <Route path="/update_account" element={<AccountUpdate />}/>
+      <Route path="/password_forget" element={<PasswordForget />}/>
+      <Route path="/password_edit" element={<PasswordEdit />}/>
+      <Route path="/hello" element={<Hello />}/>
+    </Route>
+  )
+);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <Router>
-    <CookiesProvider>
-      <UserProvider>
-        <AuthApiProvider>
-          <App />
-        </AuthApiProvider>
-      </UserProvider>
-    </CookiesProvider>
-  </Router>
+  <CookiesProvider>
+    <UserProvider>
+      <AuthApiProvider>
+        <LifelogApiProvider>
+          <RouterProvider router={router} />
+        </LifelogApiProvider>
+      </AuthApiProvider>
+    </UserProvider>
+  </CookiesProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
