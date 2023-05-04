3946e0fc5bc907e4e1d868ed9cb4f349d0e77c69
j.nishimura <junya.nishimura.0915@gmail.com>
2023-05-04 11:50:01 +0900

    loaderプロパティは使わない。

--- src/Hello.js
+++ src/Hello.js
@@ -1,21 +1,33 @@
 import './Hello.css';
 import { Button, Intent, Card, Elevation } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
-import {useLoaderData} from "react-router-dom"
 import {useUser} from "./providers/UserProvider";
 import {useEffect, useState} from "react";
 import {useAuth} from "./providers/AuthApiProvider";
+import {useLifelog} from "./providers/LifelogApiProvider";
+import {useNavigate} from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const Hello = () => {
-  const { user , clearUser } = useUser();
+  const { user , clearUser, isLogin } = useUser();
   const { authApi } = useAuth();
-  const [ valid, setValid ] = useState(false)
+  const { lifelogApi: api } = useLifelog();
+  const [ valid, setValid ] = useState(false);
   const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
-  const response = useLoaderData();
-  useEffect(()=>{
-    setMessage(response.message);
-  },[response.message])
+  useEffect(()=> {
+    if(!isLogin()) {
+      toast.error('ログインしてください。', {style: {color: 'red'}});
+      return navigate('/login');
+    }
+    api.hello()
+      .then(r => setMessage(r.data.message))
+      .catch(e => {
+        toast.error(e.message, {style: {color: 'red'}});
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleValidToken = (e) => {
     e.preventDefault();
--- src/providers/RootRouterApovider.js
+++ src/providers/RootRouterApovider.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider, redirect } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from "../App";
 import Hello from "../Hello";
 import Login from "../Login";
@@ -6,11 +6,8 @@ import SignUp from "../SignUp";
 import AccountUpdate from "../AccountUpdate";
 import PasswordForget from "../PasswordForget";
 import PasswordEdit from "../PasswordEdit";
-import {useUser} from "./UserProvider";
 
 const RootRouterProvider = () => {
-  const { user, isLogin } = useUser();
-
   const router = createBrowserRouter([
     {
       path: '/',
@@ -43,21 +40,6 @@ const RootRouterProvider = () => {
         {
           path: '/hello',
           element: <Hello/>,
-          loader: async () => {
-            if(isLogin()){
-              // axiosを使うと永久ループ？
-              // 原因はわからないが fetch でリクエストすると問題なくデータが取得できたのでさらなる検証が必要！
-              // fetch は axios のラッパーじゃないの？
-              // TODO client の見直し
-              // TODO リクエストエラー処理
-              return await fetch('http://localhost:3000/v1/test', { headers: {
-                "access-token": user.token,
-                  uid: user.uid,
-                  client: user.client
-              }});
-            }
-            return redirect('/login');
-          }
         },
       ]
     }
