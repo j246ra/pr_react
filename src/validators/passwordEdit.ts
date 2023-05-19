eb23d4d8398e0f74cf66c5fa7a1857c23a9ef125
j.nishimura <junya.nishimura.0915@gmail.com>
2023-05-19 21:29:31 +0900

    PasswordEditバリデーター TS化

--- src/validators/passwordEdit.ts
+++ src/validators/passwordEdit.ts
@@ -1,10 +1,14 @@
-import validator from './validator';
+import validator, { Result } from './validator';
 
-export default function passwordEditValidator(password, passwordConfirmation) {
+type PasswordEditValidator = (password: string, passwordConfirmation: string) => Result;
+
+const passwordEditValidator: PasswordEditValidator = (password, passwordConfirmation) => {
   const { result, addError, passwordLengthValidator } = validator();
   if (password !== undefined && password !== '')
     passwordLengthValidator(password);
   if (password !== passwordConfirmation)
     addError('入力したパスワードが一致しません。');
   return result;
-}
+};
+
+export default passwordEditValidator;
