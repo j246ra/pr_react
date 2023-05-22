95b89f864acd337d4bb59b2d9571ab2d3f943d34
j.nishimura <junya.nishimura.0915@gmail.com>
2023-05-22 23:23:02 +0900

    不要なコンポーネント削除

--- src/components/parts/InputEmail.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import InputEmail from './InputEmail';
-
-test('email form test', () => {
-  render(<InputEmail />);
-  const input = screen.getByLabelText(/メールアドレス/);
-  fireEvent.change(input, { target: { value: 'junkichii@gmail.com' } });
-  expect(input.value).toEqual('junkichii@gmail.com');
-});
