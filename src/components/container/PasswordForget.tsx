8953090cded0402959ce9710f07c5c69eda0b67d
j.nishimura <junya.nishimura.0915@gmail.com>
2023-05-23 14:03:56 +0900

    PasswordForget.tsx再設計

---
+++ src/components/container/PasswordForget.tsx
@@ -0,0 +1,43 @@
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+import { Button, Callout } from '@blueprintjs/core';
+import notify from '@lib/toast';
+import { useAuth } from '@providers/AuthApiProvider';
+import { EmailInput } from '@presentational/EmailInput';
+import { useNavigate } from 'react-router-dom';
+import SessionCard from '@presentational/SessionCard';
+
+const PasswordForget: React.FC = () => {
+  const { authApi: session } = useAuth();
+  const [email, setEmail] = useState<string>('');
+  const navigate = useNavigate();
+
+  const handlePasswordForget = (e: FormEvent) => {
+    e.preventDefault();
+    session
+      .passwordForget(email)
+      .then(() => {
+        notify.success('パスワードリセットメールを送信しました。');
+        navigate('/send_success');
+      })
+      .catch(() => {
+        notify.error('送信に失敗しました。');
+      });
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+
+  return (
+    <SessionCard>
+      <Callout className="session-callout" icon="info-sign" intent="primary">
+        パスワードリセットメールの送信先を入力してください。
+      </Callout>
+      <form onSubmit={handlePasswordForget}>
+        <EmailInput value={email} onChange={handleEmailChange} />
+        <Button type="submit" intent="primary" icon="envelope" text="送信" />
+      </form>
+    </SessionCard>
+  );
+};
+
+export default PasswordForget;
