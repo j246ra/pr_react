f18d8d62dc07a93534ebe42579eddfcade704b60
j.nishimura <junya.nishimura.0915@gmail.com>
2023-05-26 16:12:32 +0900

    EmailInputコンポーネントストーリー作成をCSF3に変換

--- src/components/presentational/EmailInput.stories.tsx
+++ src/components/presentational/EmailInput.stories.tsx
@@ -1,30 +1,31 @@
 import React, { ChangeEvent } from 'react';
-import { Story, Meta } from '@storybook/react';
-import { EmailInput, EmailInputProps } from './EmailInput';
+import { Meta } from '@storybook/react';
+import { EmailInput } from './EmailInput';
 
 export default {
   title: 'Components/EmailInput',
   component: EmailInput,
 } as Meta;
 
-const Template: Story<EmailInputProps> = (args) => <EmailInput {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  value: '',
-  onChange: (e: ChangeEvent<HTMLInputElement>) => {},
+export const Default = {
+  args: {
+    value: '',
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {},
+  },
 };
 
-export const WithPlaceholder = Template.bind({});
-WithPlaceholder.args = {
-  value: '',
-  onChange: (e: ChangeEvent<HTMLInputElement>) => {},
-  placeholder: 'メールアドレスを入力してください',
+export const WithPlaceholder = {
+  args: {
+    value: '',
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {},
+    placeholder: 'メールアドレスを入力してください',
+  },
 };
 
-export const Required = Template.bind({});
-Required.args = {
-  value: '',
-  onChange: (e: ChangeEvent<HTMLInputElement>) => {},
-  required: true,
+export const Required = {
+  args: {
+    value: '',
+    onChange: (e: ChangeEvent<HTMLInputElement>) => {},
+    required: true,
+  },
 };
