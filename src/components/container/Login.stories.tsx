dd3f6c5bbf1a7334ba52a331c2b82ff3f08a2c1d
j.nishimura <junya.nishimura.0915@gmail.com>
2023-06-29 19:28:58 +0900

    Login.stories.tsx APIモック化

--- src/components/container/Login.stories.tsx
+++ src/components/container/Login.stories.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
 import Login from '@container/Login';
+import { rest } from 'msw';
+
+export const SuccessBehavior: Meta<typeof Login> = () => <Login />;
+SuccessBehavior.parameters = {
+  msw: {
+    handlers: [
+      rest.post('http://localhost:3000/v1/auth/sign_in', (req, res, ctx) => {
+        return res(ctx.status(200));
+      }),
+    ],
+  },
+};
 
 export default {
   title: 'Container/Login',
