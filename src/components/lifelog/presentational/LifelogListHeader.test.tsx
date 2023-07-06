1fb4f36b556b11c85cbb1c5d6393b07c018583d2
j.nishimura <junya.nishimura.0915@gmail.com>
2023-07-06 16:22:38 +0900

    LifelogListHeader テストコード修正

--- src/components/lifelog/presentational/LifelogListHeader.test.tsx
+++ src/components/lifelog/presentational/LifelogListHeader.test.tsx
@@ -5,22 +5,34 @@ import LifelogListHeader from './LifelogListHeader';
 
 describe('LifelogListHeader', () => {
   it('enabled が true 時にヘッダーが表示される', () => {
-    const { container } = render(<LifelogListHeader enabled={true} />);
-    const thead = container.firstChild;
+    const { container } = render(
+      <table>
+        <LifelogListHeader enabled={true} />
+      </table>
+    );
+    const thead = container.getElementsByTagName('thead')[0];
 
     expect(thead).toBeVisible();
   });
 
   it('enabled が false 時にヘッダーが表示されない', () => {
-    const { container } = render(<LifelogListHeader enabled={false} />);
-    const thead = container.firstChild;
+    const { container } = render(
+      <table>
+        <LifelogListHeader enabled={false} />
+      </table>
+    );
+    const thead = container.getElementsByTagName('thead')[0];
 
     expect(thead).not.toBeVisible();
   });
 
   it('enabled が 未指定時にヘッダーが表示されない', () => {
-    const { container } = render(<LifelogListHeader />);
-    const thead = container.firstChild;
+    const { container } = render(
+      <table>
+        <LifelogListHeader />
+      </table>
+    );
+    const thead = container.getElementsByTagName('thead')[0];
 
     // ヘッダーが表示されることを確認します
     expect(thead).toBeVisible();
