9bb7cb8957ed9e9dbf04eaac555ef7cd525f8433
j.nishimura <junya.nishimura.0915@gmail.com>
2023-07-12 00:25:59 +0900

    BluePrint5.Icon 関連の Warning メッセージを回避

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const originalConsoleError = console.error;
+
+console.error = (...args) => {
+  if (
+    !args[0].includes(
+      'Warning: An update to %s inside a test was not wrapped in'
+    ) ||
+    args[1] !== 'Blueprint5.Icon'
+  ) {
+    originalConsoleError(...args);
+  }
+};
