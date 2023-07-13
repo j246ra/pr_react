5be09546ef4b8e01b632987329e0ac1ccd52a51f
j.nishimura <junya.nishimura.0915@gmail.com>
2023-07-13 16:40:53 +0900

    passwordEdit バリデーターで2つのパスワードが一致したときだけ長さチェックをするようにした。

--- src/validators/passwordEdit.ts
+++ src/validators/passwordEdit.ts
@@ -1,13 +1,27 @@
 import validator, { INVALID_MESSAGES, Result } from './validator';
 
-type PasswordEditValidator = (password: string, passwordConfirmation: string) => Result;
+type PasswordEditValidator = (
+  password: string,
+  passwordConfirmation: string
+) => Result;
 
-const passwordEditValidator: PasswordEditValidator = (password, passwordConfirmation) => {
-  const { result, addError, passwordLengthValidator } = validator();
-  if (password !== undefined && password !== '')
-    passwordLengthValidator(password);
-  if (password !== passwordConfirmation)
-    addError(INVALID_MESSAGES.PASSWORD_NO_MATCH);
+const passwordEditValidator: PasswordEditValidator = (
+  password,
+  passwordConfirmation
+) => {
+  const { result, addError, textPresenceValidator, passwordLengthValidator } =
+    validator();
+
+  textPresenceValidator(password, 'パスワード');
+  textPresenceValidator(passwordConfirmation, 'パスワード（確認用）');
+
+  if (!result.isInvalid) {
+    if (password !== passwordConfirmation)
+      addError(INVALID_MESSAGES.PASSWORD_NO_MATCH);
+  }
+
+  if (!result.isInvalid) passwordLengthValidator(password);
+
   return result;
 };
 
