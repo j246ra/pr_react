f01d063347d4c38e555be7b5e8b320f14c902c39
j.nishimura <junya.nishimura.0915@gmail.com>
2023-07-14 13:45:03 +0900

    toast.ts に型情報追加

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,6 +1,11 @@
 import toast from 'react-hot-toast';
 
-const notify = {
+export interface Notify {
+  error: (message: string) => void;
+  success: (message: string) => void;
+}
+
+const notify: Notify = {
   error: (message: string) => {
     toast.error(message, { style: { color: 'red ' } });
   },
