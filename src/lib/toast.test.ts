d587719d9b7b059b078a31a962b73420411fafb7
j.nishimura <junya.nishimura.0915@gmail.com>
2023-07-14 15:17:24 +0900

    toast.test.ts 追加

---
+++ src/lib/toast.test.ts
@@ -0,0 +1,19 @@
+import notify from '@lib/toast';
+import toast from 'react-hot-toast';
+
+jest.mock('react-hot-toast');
+const mockToast = jest.mocked(toast);
+describe('toast', () => {
+  it('success', () => {
+    notify.success('成功');
+    expect(mockToast.success).toHaveBeenCalled();
+    expect(mockToast.success).toHaveBeenCalledWith('成功');
+  });
+  it('error', () => {
+    notify.error('エラー');
+    expect(mockToast.error).toHaveBeenCalled();
+    expect(mockToast.error).toHaveBeenCalledWith('エラー', {
+      style: { color: 'red ' },
+    });
+  });
+});
