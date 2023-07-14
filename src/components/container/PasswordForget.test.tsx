6e40e6e41453bc9402bd7abea6e65f8cd73e4191
j.nishimura <junya.nishimura.0915@gmail.com>
2023-07-14 22:21:13 +0900

    PasswordForget.test.tsx navigateモック使用

--- src/components/container/PasswordForget.test.tsx
+++ src/components/container/PasswordForget.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import PasswordForget from './PasswordForget';
 import { useAuth } from '@providers/AuthApiProvider';
+import { mockNavigator } from '@src/tests/common';
 
 jest.mock('@providers/AuthApiProvider');
 
@@ -18,20 +18,12 @@ describe('PasswordForget コンポーネント', () => {
   });
 
   it('メールアドレス入力フォームを表示する', () => {
-    const { getByPlaceholderText } = render(
-      <Router>
-        <PasswordForget />
-      </Router>
-    );
+    const { getByPlaceholderText } = render(<PasswordForget />);
     expect(getByPlaceholderText('メールアドレスを入力')).toBeInTheDocument();
   });
 
   it('ユーザーがパスワードを忘れたときにパスワードリセットメールを送信できる', async () => {
-    const { getByPlaceholderText, getByText } = render(
-      <Router>
-        <PasswordForget />
-      </Router>
-    );
+    const { getByPlaceholderText, getByText } = render(<PasswordForget />);
 
     const emailInput = getByPlaceholderText('メールアドレスを入力');
     const submitButton = getByText('送信');
@@ -46,6 +38,7 @@ describe('PasswordForget コンポーネント', () => {
       expect(mockUseAuth().authApi.passwordForget).toHaveBeenCalledWith(
         'test@example.com'
       );
+      expect(mockNavigator).toHaveBeenCalledWith('/send_success');
     });
   });
 });
