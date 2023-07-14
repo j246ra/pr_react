ddcb4158e1102d42b64e91f335d144bfc3b80fea
j.nishimura <junya.nishimura.0915@gmail.com>
2023-07-14 23:34:09 +0900

    common.ts 不要なモジュールmock削除

--- src/tests/common.ts
+++ src/tests/common.ts
@@ -1,6 +1,4 @@
 import '@testing-library/jest-dom';
-
-jest.mock('@providers/SessionProvider');
 export const mockNavigator = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
