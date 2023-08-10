83503ed14ad3e5a23b6b6012f0dcbd090578dd86
j.nishimura <junya.nishimura.0915@gmail.com>
2023-08-10 17:42:58 +0900

    表示用日時フォーマット定義

--- src/lib/dateUtil.ts
+++ src/lib/dateUtil.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
 
 export const DATETIME_FULL = 'YYYY-MM-DD HH:mm:ss.SSS';
+export const DISPLAY_DATETIME_FULL = 'YYYY-MM-DD HH:mm:ss';
 export const days = () => dayjs();
