a9f8464613e55cf3e59fdf65238b75eb897da795
j.nishimura <junya.nishimura.0915@gmail.com>
2023-08-27 16:16:39 +0900

    LifelogEditDialog.stories.tsx 修正

--- src/components/lifelog/container/LifelogEditDialog.stories.tsx
+++ src/components/lifelog/container/LifelogEditDialog.stories.tsx
@@ -1,19 +1,29 @@
 import { Meta } from '@storybook/react';
 import LifelogEditDialog from './LifelogEditDialog';
-import { lifelog } from '@lib/faker/lifelog';
-import { lifelogMocks } from '../../../lib/storybook/lifelog';
+import { lifelogMocks } from '@lib/storybook/lifelog';
+import LifelogEditDialogProvider from '@providers/LifelogEditDialogProvider';
+import LifelogList from '@lifelog/container/LifelogList';
 
 export default {
   title: 'Lifelog/Container/LifelogEditDialog',
   component: LifelogEditDialog,
+  decorators: [
+    (Story) => (
+      <div className={'App'}>
+        <LifelogEditDialogProvider>
+          <LifelogList />
+          <Story />
+        </LifelogEditDialogProvider>
+      </div>
+    ),
+  ],
 } as Meta;
 
 const { all } = lifelogMocks();
 
 export const Default = {
   args: {
-    isOpen: true,
-    log: lifelog(),
+    detailRows: 8,
   },
   parameters: {
     msw: all(),
