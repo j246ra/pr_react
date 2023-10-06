724f78d118a29e21f2435c8737b44e22bb0635f5
j.nishimura <junya.nishimura.0915@gmail.com>
2023-10-06 14:25:55 +0900

    dateUtil.ts の Timezone を設定

--- src/lib/dateUtil.ts
+++ src/lib/dateUtil.ts
@@ -1,5 +1,12 @@
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import { DATE_UTIL as CONST } from './consts/common';
+import { DATETIME_INPUT } from '@lib/consts/component';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault(DATETIME_INPUT.TIMEZONE);
 
 export const DATETIME_FULL = CONST.FORMAT.DATETIME_FULL;
 export const DISPLAY_DATETIME = CONST.FORMAT.DISPLAY_DATETIME;
