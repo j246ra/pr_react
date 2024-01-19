30462bb7127e9a04e62c632927536b518a0351c6
j.nishimura <junya.nishimura.0915@gmail.com>
2024-01-19 17:10:51 +0900

    ContextInputコンポーネントの分割

---
+++ src/components/lifelog/container/ContextInput.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import ContextInput from './ContextInput';
+import { useLifelog } from '@providers/LifelogProvider';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { CONTEXT_INPUT } from '@lib/consts/component';
+
+jest.mock('@providers/LifelogProvider');
+
+describe('ContextInput', () => {
+  let createLogByContext: jest.Mock;
+
+  beforeEach(() => {
+    createLogByContext = jest.fn().mockResolvedValue({ status: 200 });
+    (useLifelog as jest.Mock).mockReturnValue({
+      createLogByContext,
+    });
+  });
+
+  it('renders correctly', () => {
+    render(
+      <Router>
+        <ContextInput />
+      </Router>
+    );
+
+    const input = screen.getByPlaceholderText(CONTEXT_INPUT.PLACEHOLDER);
+    expect(input).toBeInTheDocument();
+  });
+
+  it('handles form submission correctly', async () => {
+    render(
+      <Router>
+        <ContextInput />
+      </Router>
+    );
+
+    const testInput = 'Test input';
+
+    const button = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(CONTEXT_INPUT.PLACEHOLDER);
+
+    await waitFor(() => {
+      fireEvent.change(input, { target: { value: testInput } });
+      fireEvent.submit(button);
+    });
+
+    expect(createLogByContext).toHaveBeenCalledWith(testInput);
+  });
+});
---
+++ src/components/lifelog/container/ContextInput.tsx
@@ -0,0 +1,30 @@
+import React, { FormEvent, useState } from 'react';
+import { useLifelog } from '@providers/LifelogProvider';
+import notify from '@lib/toast';
+import { LIFELOGS } from '@lib/consts/component';
+import ContextInputForm from '@lifelog/presentational/ContextInputForm';
+
+const ContextInput: React.FC = () => {
+  const { createLogByContext } = useLifelog();
+  const [context, setContext] = useState<string>('');
+
+  const handleCreateLifelog = (e: FormEvent) => {
+    e.preventDefault();
+    createLogByContext(context)
+      .then(() => {
+        setContext('');
+        notify.success(LIFELOGS.MESSAGE.SUCCESS);
+      })
+      .catch((e) => notify.error(e.message));
+  };
+
+  return (
+    <ContextInputForm
+      onSubmit={handleCreateLifelog}
+      value={context}
+      onChange={(e) => setContext(e.target.value)}
+    />
+  );
+};
+
+export default ContextInput;
