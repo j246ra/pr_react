c32dd1e2b1b9f3c3ad60f33223adfa13df6d7d26
j.nishimura <junya.nishimura.0915@gmail.com>
2024-02-07 15:05:51 +0900

    JSDoc 追記

--- src/components/AuthenticatedOnly.tsx
+++ src/components/AuthenticatedOnly.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { AUTHENTICATED_ONLY } from '@lib/consts/component';
 import AuthGate, { AuthGateProps } from '@src/components/AuthGate';
 
+/**
+ * AuthenticatedOnlyProps
+ *
+ * @property children 認証済みの場合に表示するコンポーネント
+ * @property [fallbackPath] 未認証の場合のリダイレクト先、未指定の場合は AUTHENTICATED_ONLY.FALLBACK_PATH
+ * @property [fallbackMessage] 未認証の場合に表示するエラーメッセージ
+ */
+
 export type AuthenticatedOnlyProps = Pick<
   AuthGateProps,
   'children' | 'fallbackMessage'
 > &
   Partial<Pick<AuthGateProps, 'fallbackPath'>>;
-// export type AuthenticatedOnlyProps = {
-//   children: React.ReactNode;
-//   fallbackPath?: string;
-//   fallbackMessage?: string;
-// }
 
 function AuthenticatedOnly({
   children,
--- src/components/UnauthenticatedOnly.tsx
+++ src/components/UnauthenticatedOnly.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import AuthGate, { AuthGateProps } from '@src/components/AuthGate';
 import { UNAUTHENTICATED_ONLY } from '@lib/consts/component';
 
+/**
+ * UnauthenticatedOnlyProps
+ *
+ * @property children 未認証の場合に表示するコンポーネント
+ * @property [fallbackPath] 認証済みの場合のリダイレクト先、未指定の場合は UNAUTHENTICATED_ONLY.FALLBACK_PATH
+ * @property [fallbackMessage] 認証済みの場合に表示するエラーメッセージ
+ */
 export type UnauthenticatedOnlyProps = Pick<
   AuthGateProps,
   'children' | 'fallbackMessage'
 > &
   Partial<Pick<AuthGateProps, 'fallbackPath'>>;
-// export type UnauthenticatedOnlyProps = {
-//   children: React.ReactNode;
-//   fallbackPath?: string;
-//   fallbackMessage?: string;
-// }
 
 function UnauthenticatedOnly({
   children,
