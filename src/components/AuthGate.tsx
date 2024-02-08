f417849f3403ad15e15d21c17df3f3ed7b2b5582
j.nishimura <junya.nishimura.0915@gmail.com>
2024-02-08 15:28:06 +0900

    AuthGate.tsx リファクタリング

--- src/components/AuthGate.tsx
+++ src/components/AuthGate.tsx
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react';
 import { useUser } from '@providers/UserProvider';
 import { useNavigate } from 'react-router-dom';
 import notify from '@lib/toast';
-
+import { EmptyComponent } from '@src/components/EmptyComponent';
 export type AuthGateProps = {
   children: React.ReactNode;
   passingCondition: (isLoggedIn: boolean) => boolean;
   fallbackPath: string;
   fallbackMessage?: string;
 };
-
 function AuthGate({
   children,
   passingCondition,
@@ -18,16 +17,13 @@ function AuthGate({
 }: AuthGateProps) {
   const { isLoggedIn } = useUser();
   const navigate = useNavigate();
-
+  const pass = passingCondition(isLoggedIn());
   useEffect(() => {
-    if (!passingCondition(isLoggedIn())) {
+    if (!pass) {
       notify.error(fallbackMessage);
       navigate(fallbackPath);
-      return;
     }
-  }, []);
-
-  if (passingCondition(isLoggedIn())) return children;
+  }, [pass]);
+  return pass ? children : <EmptyComponent />;
 }
-
 export default AuthGate;
