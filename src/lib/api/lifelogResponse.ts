b1e67d91b5da17d60edb2ae2ade1f2ab994fe8df
j.nishimura <junya.nishimura.0915@gmail.com>
2024-02-14 16:23:05 +0900

    lifelogResponse.ts リファクタリング

--- src/lib/api/lifelogResponse.ts
+++ src/lib/api/lifelogResponse.ts
@@ -7,6 +7,31 @@ type DataWithErrors = {
   errors: string[];
 };
 
+type ConfigValueType =
+  | 'number'
+  | 'string'
+  | 'boolean'
+  | 'null'
+  | 'object'
+  | 'array'
+  | 'unknown';
+type ConfigValue = [ConfigValueType, boolean];
+const typeCheckConfigurations: Record<string, ConfigValue> = {
+  id: ['number', false],
+  userId: ['number', false],
+  action: ['string', false],
+  detail: ['string', true],
+  startedAt: ['string', false],
+  finishedAt: ['string', true],
+  createdAt: ['string', false],
+  updatedAt: ['string', false],
+};
+
+const convertConfigToObject = (config: ConfigValue) => {
+  const [expectedType, isNullable] = config;
+  return { expectedType, isNullable };
+};
+
 export const validateResponseData = (
   data: any
 ): { validData: BaseLifelog[]; invalidData: DataWithErrors[] } => {
@@ -19,21 +44,11 @@ export const validateResponseData = (
   for (const item of items) {
     const errors: string[] = [];
 
-    if (typeof item.id !== 'number') errors.push(`Invalid id [${item.id}]`);
-    if (typeof item.userId !== 'number')
-      errors.push(`Invalid userId [${item.userId}]`);
-    if (typeof item.action !== 'string')
-      errors.push(`Invalid action [${item.action}]`);
-    if (item.detail && typeof item.detail !== 'string')
-      errors.push(`Invalid detail [${item.detail}]`);
-    if (typeof item.startedAt !== 'string')
-      errors.push(`Invalid startedAt [${item.startedAt}]`);
-    if (item.finishedAt && typeof item.finishedAt !== 'string')
-      errors.push(`Invalid finishedAt [${item.finishedAt}]`);
-    if (typeof item.createdAt !== 'string')
-      errors.push(`Invalid createdAt [${item.createdAt}]`);
-    if (typeof item.updatedAt !== 'string')
-      errors.push(`Invalid updatedAt [${item.updatedAt}]`);
+    Object.entries(typeCheckConfigurations).forEach(([key, config]) => {
+      const { expectedType, isNullable } = convertConfigToObject(config);
+      if ((item[key] || !isNullable) && typeof item[key] !== expectedType)
+        errors.push(`Invalid ${key} [${item[key]}]`);
+    });
 
     if (errors.length > 0) invalidData.push({ data: item, errors });
     else validData.push(item);
