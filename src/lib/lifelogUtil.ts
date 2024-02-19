62744817d32c28f2fe3958e4ce38189e6655f5d0
j.nishimura <junya.nishimura.0915@gmail.com>
2024-02-19 22:40:42 +0900

    1行入力で先頭が数字の場合は開始日時として扱う

--- src/lib/lifelogUtil.ts
+++ src/lib/lifelogUtil.ts
@@ -1,6 +1,5 @@
 import { Lifelog } from '@providers/LifelogProvider';
 import { DATETIME_FULL, days } from '@lib/dateUtil';
-import { CreatParams } from '@lib/api/lifelog';
 
 export const blank = (): Lifelog => {
   return {
@@ -32,20 +31,40 @@ export const sort = (logs: Lifelog[]) => {
 };
 
 export const buildCreateParamsByContext = (context: string) => {
-  const params: Required<CreatParams> = {
-    action: context,
-    detail: null,
-    startedAt: days().format(DATETIME_FULL),
-    finishedAt: null,
-  };
+  let startedAt: string | null = null;
+
+  // 入力が時間（HHmm形式）で始まるかどうかを確認する正規表現
+  const timeRegEx = /^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]\b/;
+  const result = context.match(timeRegEx);
+
+  // 入力が時間で始まる場合、それを開始時刻として扱う
+  if (result) {
+    const time = result[0];
+    const today = days().format('YYYY-MM-DD');
+    startedAt = days(today)
+      .hour(Number(time.slice(0, 2)))
+      .minute(Number(time.slice(2, 4)))
+      .format(DATETIME_FULL);
+    // 入力から時間部分を削除
+    context = context.replace(timeRegEx, '').trim();
+  }
+
   // 正規表現で全角半角の空白を検出
   const regex = /[\s\u3000]/;
   const index = context.search(regex);
+  let action = context;
+  let detail = null;
   if (index !== -1) {
-    params.action = context.slice(0, index);
-    params.detail = context.slice(index + 1) || null;
+    action = context.slice(0, index);
+    detail = context.slice(index + 1) || null;
   }
-  return params;
+
+  return {
+    action: action,
+    detail: detail,
+    startedAt: startedAt || days().format(DATETIME_FULL),
+    finishedAt: null,
+  };
 };
 
 export const lifelogUtil = {
