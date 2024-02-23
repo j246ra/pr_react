acb28d4f4183ea7723dae4879134c43ee28d1417
j.nishimura <junya.nishimura.0915@gmail.com>
2024-02-23 13:42:05 +0900

    文字列省略処理を共通化

--- src/components/lifelog/presentational/LifelogListItemSp.tsx
+++ src/components/lifelog/presentational/LifelogListItemSp.tsx
@@ -3,6 +3,8 @@ import { LifelogListItemProps } from '@lifelog/presentational/LifelogListItem';
 import { EntityTitle } from '@blueprintjs/core';
 import styles from './LifelogListItem.module.scss';
 import useActionTimeDisplay from '@src/hooks/useActionTimeDisplay';
+import { truncate } from '@lib/stringUtil';
+import { LIFELOG_LIST_ITEM_SP as Defs } from '@lib/consts/component';
 
 type LifelogListItemSpProps = Omit<LifelogListItemProps, 'onActionClick'>;
 
@@ -11,13 +13,10 @@ export function LifelogListItemSp({
   onEditButtonClick,
 }: LifelogListItemSpProps) {
   const { displayDatetime, displayActionTime } = useActionTimeDisplay(log);
-  const detail = useMemo(() => {
-    if (log.detail)
-      return log.detail.length > 52
-        ? `${log.detail?.slice(0, 52)}...`
-        : log.detail;
-    return null;
-  }, [log.detail]);
+  const detail = useMemo(
+    () => truncate(log.detail, Defs.DETAIL_TRUNCATE_LENGTH),
+    [log.detail]
+  );
 
   return (
     <tr className={styles.trItem}>
