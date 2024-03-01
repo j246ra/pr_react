583e46f93f535d86ddc36e253f2a2bec10248411
j.nishimura <junya.nishimura.0915@gmail.com>
2024-03-01 14:13:55 +0900

    useMediaQuery のテストコード修正

--- src/hooks/useMediaQuery.test.ts
+++ src/hooks/useMediaQuery.test.ts
@@ -1,41 +1,15 @@
 import { renderHook } from '@testing-library/react';
 import useMediaQuery, { mediaQuery } from '@src/hooks/useMediaQuery';
+import { matchMediaObject } from '@src/tests/matchMedia';
 describe('useMediaQuery', () => {
-  const mediaQueryList = ({ matches = true, media = '' }) => {
-    return {
-      matches,
-      media,
-      onchange: null,
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    };
-  };
-  beforeAll(() => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest
-        .fn()
-        .mockImplementation((query) =>
-          mediaQueryList({ matches: true, media: query })
-        ),
-    });
-  });
-
   it('メディアクエリがマッチした場合にtrueを返す', () => {
-    // @ts-ignore
-    window.matchMedia.mockImplementation((query) =>
-      mediaQueryList({ matches: true, media: query })
-    );
+    matchMediaObject({ matches: true });
     const { result } = renderHook(() => useMediaQuery(mediaQuery.pc));
     expect(result.current).toBe(true);
   });
 
   it('メディアクエリがマッチしない場合にfalseを返す', () => {
-    // @ts-ignore
-    window.matchMedia.mockImplementation((query) =>
-      mediaQueryList({ matches: false, media: query })
-    );
+    matchMediaObject({ matches: false });
     const { result } = renderHook(() => useMediaQuery(mediaQuery.pc));
     expect(result.current).toBe(false);
   });
