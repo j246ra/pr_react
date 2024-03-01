97c4906593c990857d71b022345d1866f0367427
j.nishimura <junya.nishimura.0915@gmail.com>
2024-03-01 18:16:25 +0900

    useLifelogList テスト追加

--- src/hooks/useLifelogList.test.ts
+++ src/hooks/useLifelogList.test.ts
@@ -3,6 +3,7 @@ import useLifelogList from '@src/hooks/useLifelogList';
 import { LIFELOG_LIST } from '@lib/consts/component';
 import toast from 'react-hot-toast';
 import { lifelog } from '@lib/faker/lifelog';
+import { act, renderHook, waitFor } from '@testing-library/react';
 
 jest.mock('@providers/LifelogProvider');
 const mockUseLifelog = useLifelog as jest.MockedFunction<any>;
@@ -38,27 +39,55 @@ describe('useLifelogList', () => {
       isTerminated: false,
     });
   });
-  it('lifelogLoader 正常系', async () => {
-    const { lifelogLoader } = useLifelogList();
-    await lifelogLoader();
-    expect(mockUseLifelog().loadLogs).toHaveBeenCalledWith(
-      LIFELOG_LIST.MESSAGE.ERROR
-    );
-    expect(mockToast.error).not.toHaveBeenCalled();
+  describe('lifelogLoader', () => {
+    it('正常系', async () => {
+      const { result } = renderHook(useLifelogList);
+      await result.current.lifelogLoader();
+      expect(mockUseLifelog().loadLogs).toHaveBeenCalledWith(
+        LIFELOG_LIST.MESSAGE.ERROR
+      );
+      expect(mockToast.error).not.toHaveBeenCalled();
+    });
+
+    it('取得データ異常', async () => {
+      mockUseLifelog().loadLogs = jest
+        .fn()
+        .mockReturnValue(Promise.resolve(invalidResponse()));
+      const { result } = renderHook(useLifelogList);
+      await result.current.lifelogLoader();
+      expect(mockUseLifelog().loadLogs).toHaveBeenCalledWith(
+        LIFELOG_LIST.MESSAGE.ERROR
+      );
+      expect(mockToast.error).toHaveBeenCalledWith(
+        LIFELOG_LIST.MESSAGE.INVALID_DATA,
+        expect.anything()
+      );
+    });
   });
 
-  it('lifelogLoader 取得データ異常', async () => {
-    mockUseLifelog().loadLogs = jest
-      .fn()
-      .mockReturnValue(Promise.resolve(invalidResponse()));
-    const { lifelogLoader } = useLifelogList();
-    await lifelogLoader();
-    expect(mockUseLifelog().loadLogs).toHaveBeenCalledWith(
-      LIFELOG_LIST.MESSAGE.ERROR
-    );
-    expect(mockToast.error).toHaveBeenCalledWith(
-      LIFELOG_LIST.MESSAGE.INVALID_DATA,
-      expect.anything()
-    );
+  describe('hasMore', () => {
+    it('true -> false', async () => {
+      const { result, rerender } = renderHook(useLifelogList);
+      expect(result.current.hasMore).toEqual(true);
+      act(() => {
+        mockUseLifelog().isTerminated = true;
+      });
+      rerender();
+      await waitFor(() => {
+        expect(result.current.hasMore).toEqual(false);
+      });
+    });
+    it('false -> true', async () => {
+      mockUseLifelog().isTerminated = true;
+      const { result, rerender } = renderHook(useLifelogList);
+      expect(result.current.hasMore).toEqual(false);
+      act(() => {
+        mockUseLifelog().isTerminated = false;
+      });
+      rerender();
+      await waitFor(() => {
+        expect(result.current.hasMore).toEqual(true);
+      });
+    });
   });
 });
