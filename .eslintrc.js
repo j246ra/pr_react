d1377862fc3769d88b59c846938b9956de0713b7
j.nishimura <junya.nishimura.0915@gmail.com>
2024-11-08 17:21:02 +0900

    .eslintrc.js 記述漏れ対応

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
+    "plugin:react/recommended",
     'plugin:@typescript-eslint/recommended',
     'plugin:storybook/recommended',
   ],
