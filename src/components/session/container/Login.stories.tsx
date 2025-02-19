33508b028c440c2e7975ffe834bf91cb55a672fd
j.nishimura <junya.nishimura.0915@gmail.com>
2025-02-19 10:55:52 +0900

    wip msw2 導入[ci skip]

--- src/components/session/container/Login.stories.tsx
+++ src/components/session/container/Login.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
 import Login from './Login';
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 import { apiHost } from '@lib/storybook/util';
 
 export const SuccessBehavior: Meta<typeof Login> = () => <Login />;
 SuccessBehavior.parameters = {
   msw: {
     handlers: [
-      rest.post(apiHost('/auth/sign_in'), (req, res, ctx) => {
-        return res(ctx.status(200));
+      http.post(apiHost('/auth/sign_in'), ({}) => {
+        return new HttpResponse(null, { status: 200 });
       }),
     ],
   },
